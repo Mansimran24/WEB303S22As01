795650bc54b3d07eb9f4ca51f7b9131a50f74a84
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-05-23 21:02:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+	WEB 303 Assignment 1 - jQuery
+	{Mansimran Kaur}
+*/
+$(document).ready(function(){
+	$("label").keyup(function(){
+		const P = $("#price").val();
+	const spanTax = (P * 13)/100;
+	$("#tax").text("$" + spanTax.toFixed(2));
+  });
+
+});
+
